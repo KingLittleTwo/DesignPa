50e1f4b29e124d18a53b445cd57bc15de26048b2
wangning <wangning@yongche.com>
2018-03-21 14:21:29 +0800

    update

--- FactoryMethod/SimpleFactory.php
+++ FactoryMethod/SimpleFactory.php
@@ -9,16 +9,16 @@ class SimpleFactory
     protected $typeList;
     public function __construct()
     {
-        $this->typeList = [
+        self::$typeList = [
             'mysql'  => __NAMESPACE__ .'\DBMysql',
             'sqlite' => __NAMESPACE__ .'\DBSqlite',
-        ]
+        ];
     }
     public static function createDB($type)
     {
-        if (!array_key_exists($type, $this->typeList))
+        if (!array_key_exists($type, self::$typeList))
             throw new Exception("$type is not valid DB");
-        $className = $this->typeList[$type];
+        $className = self::$typeList[$type];
         return new $className();
     }
 }
